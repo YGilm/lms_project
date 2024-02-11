86c8f9eaf5054bd1e4290a9f68f6a4bdddcddff5
yury gilm <ygilm@bk.ru>
2024-02-11 20:05:21 +0400

    Added new urls and router

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from users.apps import UsersConfig
+from users.views import UserViewSet
 
 app_name = UsersConfig.name
 
-urlpatterns = [
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
 
+urlpatterns = [
+    path('', include(router.urls)),
 ]
