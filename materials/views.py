5c1ba7425848bc62ccd400b683be275815a9fcca
yury gilm <ygilm@bk.ru>
2024-02-22 06:57:29 +0400

    Added permissions

--- materials/views.py
+++ materials/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, permissions
+from rest_framework.permissions import IsAuthenticated
+
 from materials.models import *
+from materials.permissions import IsModeratorOrReadOnly
 from materials.serializers import *
 
 
@@ -8,26 +11,39 @@ class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve', 'update', 'partial_update']:
+            permission_classes = [IsModeratorOrReadOnly]
+        else:
+            permission_classes = [permissions.IsAdminUser]
+        return [permission() for permission in permission_classes]
+
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [permissions.IsAdminUser]
 
 
 class LessonListAPIView(generics.ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsModeratorOrReadOnly]
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsModeratorOrReadOnly]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsModeratorOrReadOnly]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [permissions.IsAdminUser]
+
