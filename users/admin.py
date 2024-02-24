81dda773e012cc363e6a67e2f6033b4b06acddef
yury gilm <ygilm@bk.ru>
2024-02-24 14:10:58 +0400

    Changed

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from django import forms
 from django.contrib.auth.models import Group
-from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from users.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = ('email', 'phone', 'city', 'avatar',)
     list_filter = ('city',)
     search_fields = ('email', 'phone', 'city',)
+    ordering = ('email',)
 
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('phone', 'city', 'avatar')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups')}),
+    )
