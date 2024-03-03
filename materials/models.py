69c93da238c971cfba389351f2de236837ba6f08
yury gilm <ygilm@bk.ru>
2024-03-03 17:31:39 +0300

    Added new model Subscription

--- materials/models.py
+++ materials/models.py
@@ -33,3 +33,16 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = 'урок'
         verbose_name_plural = 'уроки'
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='subscriptions')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='subscribers')
+
+    class Meta:
+        unique_together = ('user', 'course')
+        verbose_name = 'подписка'
+        verbose_name_plural = 'подписки'
+
+    def __str__(self):
+        return f'{self.user} подписан на курс {self.course.title}'
